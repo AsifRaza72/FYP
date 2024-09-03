57e38c4a90ac9811b348956f81fc1142afef1b88
AsifRaza72 <asifaligohari572@gmail.com>
2024-09-03 23:25:28 +0500

    add profile model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=255)
+    roll_no = models.CharField(max_length=50, unique=True)
+    cnic = models.CharField(max_length=15, unique=True)
+    father_name = models.CharField(max_length=255)
+    father_cnic = models.CharField(max_length=15)
+    address = models.TextField()
+    department = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
